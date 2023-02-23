d3362496da64c246a057b46da8a6309986f0d429
xinyang <1349746992@qq.com>
2023-02-23 21:51:33 +0800

    master commit

--- src/newLearning/GitTest.java
+++ src/newLearning/GitTest.java
@@ -8,6 +8,6 @@ public class GitTest {
         System.out.println("master commit ");
         System.out.println("hello git3");
         System.out.println("hot-fix test");
-
+        System.out.println("push test");
     }
 }
